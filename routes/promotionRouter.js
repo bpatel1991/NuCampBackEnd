df524c7d7fffa09308de32a7ccd60247e73592a1
Bhavini Tina Patel <bhavini.patel91@gmail.com>
2021-06-04 21:14:51 -0700

    CORS

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const Promotion = require('../models/promotion');
-const authenticate = require('../authenticate')
+const express = require("express");
+const Promotion = require("../models/promotion");
+const authenticate = require("../authenticate");
+const cors = require("./cors");
+
 const promotionRouter = express.Router();
 
 promotionRouter.route('/')
-.get((req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req, res, next) => {
     Promotion.find()
-    .then(promotions => {
+      .then((promotions) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(promotions);
     })
-    .catch(err => next(err));
+    .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.create(req.body)
     .then(promotion => {
         console.log('Promotion Created ', promotion);
@@ -23,11 +27,11 @@ promotionRouter.route('/')
     })
     .catch(err => next(err));
 })
-.put(authenticate.verifyUser, (req, res) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
     Promotion.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -39,7 +43,8 @@ promotionRouter.route('/')
 
 //by promotionId//
 promotionRouter.route('/:promotionId')
-.get((req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.cors, (req, res, next) => {
     Promotion.findById(req.params.promotionId)
     .then(promotion => {
         res.statusCode = 200;
@@ -48,11 +53,11 @@ promotionRouter.route('/:promotionId')
     })
     .catch(err => next(err));
 })
-.post(authenticate.verifyUser, (req, res) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
 })
-.put(authenticate.verifyUser, authenticate.verifyUser,(req, res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin,(req, res, next) => {
     Promotion.findByIdAndUpdate(req.params.promotionId, {
         $set: req.body
     }, { new: true })
@@ -63,7 +68,7 @@ promotionRouter.route('/:promotionId')
     })
     .catch(err => next(err));
 })
-.delete(authenticate.verifyUser, authenticate.verifyUser, (req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.findByIdAndDelete(req.params.promotionId)
     .then(response => {
         res.statusCode = 200;
--- routes/users.js
+++ routes/users.js
@@ -3,74 +3,104 @@ const User = require('../models/user');
 const passport = require('passport');
 const authenticate = require('../authenticate');
 const router = express.Router();
+const cors = require('./cors');
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-  User.find()
-  .then((users) => {
-    res.statusCode= 200;
-    res.json(users);
-    console.log(users)
-  })
-});
+router.get(
+  "/",
+  cors.corsWithOptions,
+  authenticate.verifyUser,
+  authenticate.verifyAdmin,
+  function (req, res, next) {
+    User.find()
+      .then((users) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(users);
+      })
+      .catch((err) => next(err));
+  }
+);
 
 
 
-//allows new user to register on website//
-router.post('/signup', (req, res) => {
-  //check to make sure that user name is not already taken//
+router.post("/signup", cors.corsWithOptions, (req, res) => {
   User.register(
-    new User({username: req.body.username}),
+    new User({ username: req.body.username }),
     req.body.password,
     (err, user) => {
-        if (err) {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({ err: err });
+      } else {
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+        }
+        if (req.body.lastname) {
+          user.lastname = req.body.lastname;
+        }
+        user.save((err) => {
+          if (err) {
             res.statusCode = 500;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({err: err});
-        } else {
-            if (req.body.firstname) {
-                user.firstname = req.body.firstname;
-            }
-            if (req.body.lastname) {
-                user.lastname = req.body.lastname;
-            }
-            user.save(err => {
-                if (err) {
-                    res.statusCode = 500;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json({err: err});
-                    return;
-                }
-                passport.authenticate('local')(req, res, () => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json({success: true, status: 'Registration Successful!'});
-                });
-            });
+            res.setHeader("Content-Type", "application/json");
+            res.json({ err: err });
+            return;
           }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registration Successful!" });
+          });
+        });
       }
+    }
   );
 });
-//login//
-router.post('/login', passport.authenticate('local'), (req, res) => {
-  const token = authenticate.getToken({_id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
-});
 
-router.get('/logout', (req, res, next) => {
-  //if session exists, destroy session and clear cookies//
+router.post(
+  "/login",
+  cors.corsWithOptions,
+  passport.authenticate("local"),
+  (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!",
+    });
+  }
+);
+
+router.get("/logout", cors.corsWithOptions, (req, res, next) => {
   if (req.session) {
-      req.session.destroy();
-      res.clearCookie('session-id');
-      res.redirect('/');
-  //if session does not exist (they are trying to log out but they aren't even logged in..)...let know they are not logged in..//    
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
   } else {
-      const err = new Error('You are not logged in!');
-      err.status = 401;
-      return next(err);
+    const err = new Error("You are not logged in!");
+    err.status = 401;
+    return next(err);
   }
 });
+
+router.get(
+  "/facebook/token",
+  passport.authenticate("facebook-token"),
+  (req, res) => {
+    if (req.user) {
+      const token = authenticate.getToken({ _id: req.user._id });
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json({
+        success: true,
+        token: token,
+        status: "You are successfully logged in!",
+      });
+    }
+  }
+);
 
 module.exports = router;
